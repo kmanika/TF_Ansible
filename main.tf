90121368f8bba1f983877fc3e2f12b7b02c53510
kmanika <kommuri.mani@gmail.com>
2022-10-31 20:24:44 +0530

    Auto commit

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "web-sg" {
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
-]
+    ]
   egress {
     from_port     = 0
     protocol      = "-1"
